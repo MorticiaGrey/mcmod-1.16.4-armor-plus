9b6c85fccd4dd3b55f324da61dd4c91333c878b1
Morticia <sunseed218@gmail.com>
2020-12-12 15:43:52 -0700

    Setup

---
+++ src/main/java/com/Morticia/armorplus/ArmorPlus.java
@@ -0,0 +1,12 @@
+package com.Morticia.armorplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArmorPlus implements ModInitializer {
+    public static final String MOD_ID = "armorplus";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
